78426e52d80b739f471a51fc31215990ab0f5333
Urian Chang <urianchang@gmail.com>
2017-06-16 17:36:43 -0700

    Started working on body and making it responsive

--- client/assets/app.js
+++ client/assets/app.js
@@ -3,10 +3,10 @@ var myApp = angular.module('myApp', ['ngRoute']);
 //  use the config method to set up routing:
 myApp.config(function ($routeProvider) {
   $routeProvider
-    // .when('/', {
-    //     templateUrl: 'partials/all.html',
-    //     controller: 'newController'
-    // })
+    .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'homeController'
+    })
     // .when('/new', {
     //     templateUrl: 'partials/new.html',
     //     controller: 'newController'
