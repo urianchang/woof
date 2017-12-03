11ae4dc7987a6826b7a6218d329e1a6b84ed1b36
Urian <uchang@sightmachine.com>
2017-12-03 04:16:58 -0800

    Added responsive Navigation Bar component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return(
-      <h1>woof woof</h1>
+      <div>
+        <h1>Home</h1>
+      </div>
     );
   }
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,15 +1,89 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import FontAwesome from 'react-fontawesome';
+import NavigationMenu from './nav_menu.jsx';
+import './nav.css';
 
-export default class Navbar extends Component {
+class NavigationBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isSlideOut: false,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+    this.slide = this.slide.bind(this);
+    this.getDimensions = this.getDimensions.bind(this);
+  }
+
+  getDimensions(e) {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    // console.log(`Dimensions: ${this.state.width} W x ${this.state.height} H`);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.getDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.getDimensions);
+  }
+
+  slide() {
+    let s = !this.state.isSlideOut;
+    this.setState({ isSlideOut: s });
+  }
+
   render() {
+    const isSlideOut = this.state.isSlideOut;
+    const isMobile = (this.state.width <= 600) ? "mobile" : "";
     return(
-      <div className="Navbar">
-        <ul>
-          <li><Link className="Navlink" to="/">Home</Link></li>
-        </ul>
+      <div className={"nav-bar " + isMobile}>
+        <FontAwesome
+          className="nav-icon"
+          name='bars'
+          size='2x'
+          onClick={this.slide}
+          />
+        <NavigationMenu links={NavbarLinks} isSlideOut={isSlideOut} />
       </div>
     );
   }
 }
+
+const NavbarLinks = [
+  {
+    'url': "/",
+    'exact': true,
+    'title': "Home"
+  },
+  {
+    'url': "/about",
+    'exact': false,
+    'title': "About"
+  },
+  {
+    'url': "/adopt",
+    'exact': false,
+    'title': "Adopt"
+  },
+  {
+    'url': "/foster",
+    'exact': false,
+    'title': "Foster"
+  },
+  {
+    'url': "/donate",
+    'exact': false,
+    'title': "Donate"
+  },
+  {
+    'url': "/contact",
+    'exact': false,
+    'title': "Contact"
+  },
+];
+
+export default NavigationBar;
