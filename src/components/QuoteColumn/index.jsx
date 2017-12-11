4a08ac38621f1e0ffad26c1e1d62b4f37d729abe
Urian <uchang@sightmachine.com>
2017-12-10 22:59:58 -0800

    Fleshed out quote column component

--- src/components/QuoteColumn/index.jsx
+++ src/components/QuoteColumn/index.jsx
@@ -5,7 +5,14 @@ export default class QuoteColumn extends Component {
   render() {
     return(
       <div className="four columns quote-block">
-        <h3>QUOTE</h3>
+        <p className="quote-text">
+          <span className="quote-quotation">&ldquo;</span>
+          A dog is the only thing on earth that loves you more than
+          you love yourself.&rdquo;
+        </p>
+        <p className="quote-author">
+          &#8211; Josh Billings
+        </p>
       </div>
     );
   }
