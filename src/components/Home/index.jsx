cca62c8b99c4f5041ec12d892d017295b91bc53b
Urian <uchang@sightmachine.com>
2017-12-10 23:00:13 -0800

    Fleshed out home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PhotoHeader from '../PhotoHeader';
 import QuoteColumn from '../QuoteColumn';
+import leash from './leash.png';
+import './home.css';
 
 export default class Home extends Component {
   render() {
@@ -9,17 +11,22 @@ export default class Home extends Component {
         <PhotoHeader />
         <div class="row">
           <QuoteColumn />
-          <div className="eight columns">
-            <h1>Home</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+          <div className="eight columns post">
+            <div className="post-title">
+              <img src={leash} alt="leash icon" />
+              <h1>Unconditional love</h1>
+            </div>
+            <div className="post-overview">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
           </div>
         </div>
       </div>
