cfa1184b8c15c6a7b25b731fb2062b4989957901
Urian <uchang@sightmachine.com>
2017-12-17 22:40:16 -0800

    Added dog profiles to Adopt page

--- src/components/Adopt/dogs.js
+++ src/components/Adopt/dogs.js
@@ -1,8 +1,20 @@
 const DOGS = [
   {
-    "name": "Zoe",
-    "gender": "female"
-  }
+    "name": "Dog1",
+    "gender": "female",
+    "age": "Young",
+    "breed": "breed?",
+    "size": "Medium",
+    "profile": "https://www.what-dog.net/Images/faces2/scroll0015.jpg"
+  },
+  {
+    "name": "Dog2",
+    "gender": "female",
+    "age": "Young",
+    "breed": "breed?",
+    "size": "Medium",
+    "profile": "https://www.what-dog.net/Images/faces2/scroll0015.jpg"
+  },
 ];
 
 module.exports = DOGS
--- src/components/Adopt/index.jsx
+++ src/components/Adopt/index.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import DOGS from './dogs.js';
+import './adopt.css';
 
 export default class Adopt extends Component {
+  constructor() {
+    super();
+    this.makeDogProfiles = this.makeDogProfiles.bind(this);
+  }
+
+  makeDogProfiles(params, i) {
+    return(
+      <div key={i} className="dog-profile">
+        <img src={params['profile']}
+          alt={params['name']  + 'photo'} />
+        <h3>
+          <a href={"#/adopt/"+params['name']}>
+            {params['name']}
+          </a>
+        </h3>
+        <ul>
+          <li><span>Gender:</span> {params['gender']}</li>
+          <li><span>Age:</span> {params['age']}</li>
+          <li><span>Breed:</span> {params['breed']}</li>
+          <li><span>Size:</span> {params['size']}</li>
+        </ul>
+      </div>
+    );
+  }
+
   render() {
-    console.log(DOGS);
     return(
       <div className="container">
-        <div className="row">
+        <div className="row adopt-title">
           <h1>Looking for a forever home</h1>
+        </div>
+        <div className="row">
+          {DOGS.map(this.makeDogProfiles)}
         </div>
       </div>
     );
