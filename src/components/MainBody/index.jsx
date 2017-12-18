af1a40ef0a98eeea4159d59ba2d93947d88c39c3
Urian <uchang@sightmachine.com>
2017-12-18 00:01:04 -0800

    Added NoMatch component

--- src/components/MainBody/index.jsx
+++ src/components/MainBody/index.jsx
@@ -3,6 +3,7 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import './NoMatch.css';
 
 import Home from '../Home';
 import About from '../About';
@@ -17,7 +18,7 @@ export default class BodyRoutes extends Component {
         <Route exact path='/about' component={About} />
         <Route exact path='/adopt' component={Adopt} />
         <Route path='/adopt/:name' component={DogPage} />
-        <Route path='/:route' component={Placeholder} />
+        <Route path='/:route' component={NoMatch} />
       </Switch>
     );
   }
@@ -31,5 +32,13 @@ const Placeholder = ({ match }) => (
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       </p>
     </div>
+  </div>
+);
+
+const NoMatch = ({ match }) => (
+  <div className="NoMatch-body">
+    <h1>(┛◉Д◉)┛彡┻━┻</h1>
+    <h2>Page not found for /{match.params.route}!</h2>
+    <p>Maybe the page you are looking for has been removed, or you typed the wrong URL.</p>
   </div>
 );
