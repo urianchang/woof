c8f37a0d8c6420f413429b1dd7ec2166cce43f97
Urian <uchang@sightmachine.com>
2017-12-19 01:15:47 -0800

    Worked on some wording for the donation page

--- src/components/Donate/index.jsx
+++ src/components/Donate/index.jsx
@@ -8,11 +8,11 @@ export default class Donate extends Component {
         <div className="row">
           <h1>Ways to Give</h1>
           <p>
-            We greatly appreciate your consideration and generosity.
+            There are many ways to support Woofie's Rescue.
+            We greatly appreciate your consideration and generosity!
           </p>
         </div>
         <div className="row">
-          <h2>Amazon</h2>
           <h3>AmazonSmile</h3>
           <p>
             Please sign up for AmazonSmile and select "Woofies Rescue" as your
@@ -23,13 +23,12 @@ export default class Donate extends Component {
           </p>
           <h3>Amazon Wish List</h3>
           <p>
-            Our Amazon wish list is constantly updated with necessities for the rescued
-            dogs. Dog toys are greatly appreciated and much enjoyed by all the dogs!
+            Our Amazon wish list is constantly updated with supplies needed by the rescues.
           </p>
         </div>
         <div className="row">
-          <h2>PayPal</h2>
-          <p>Short description of PayPal donations</p>
+          <h3>PayPal</h3>
+          <p>A donation of any amount makes a big difference.</p>
           <p>Donate button - refer to PayPal documentation on applying for button</p>
         </div>
       </div>
